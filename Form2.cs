2f68c5cf9586fe1797482c1c4a3d23a213ede366
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-15 12:32:29 +0300

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KidFN
+{
+    public partial class Form2 : Form
+    {
+       public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string code = "textBox1.Text";
+            string path = "\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Binaries\\Win64\\FortniteLauncher.exe";
+            string args = "-epicapp=Fortnite -epicenv=Prod -epicportal -epiclocale=en-us -skippatchcheck -NOSSLPINNING -FORCECONSOLE";
+            System.Diagnostics.Process.Start(path, args);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Binaries\\Win64\\FortniteLauncher.exe");
+        }
+    }
+}
